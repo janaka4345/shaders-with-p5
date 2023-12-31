227a812709dde2eb0493a39d5173f2b81c8a1193
banuka <banukaramanayaka76@gmail.com>
2023-12-31 17:04:19 +0530

    initial:

---
+++ src/components/Canvas.jsx
@@ -0,0 +1,40 @@
+import { useEffect, useMemo, useRef, useState } from "react";
+import { ReactP5Wrapper } from "@p5-wrapper/react";
+
+export default function Canvas(props) {
+  const [t, setT] = useState(0);
+
+  return (
+    <div>
+      <div>
+        <ReactP5Wrapper sketch={sketch} />
+      </div>
+      <button onClick={() => setT((prev) => (prev += 1))}>click</button>
+      <h1>{t}</h1>
+    </div>
+  );
+}
+
+function sketch(p5) {
+  p5.preload = preload(p5);
+  p5.setup = setup(p5);
+  p5.draw = draw(p5);
+  p5.mousePressed = () => mousePressed(p5);
+}
+function setup(p5) {
+  return () => {
+    p5.pixelDensity(1);
+    p5.createCanvas(cw, ch, { willReadFrequently: true });
+  };
+}
+function preload(p5) {}
+function draw(p5) {
+  return () => {
+    p5.background(0, 0, 0);
+
+    // p5.noLoop();
+  };
+}
+function mousePressed(p5) {
+  console.log(p5.frameRate());
+}
